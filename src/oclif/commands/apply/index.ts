b5b236995841dfd8051e2b1c04b24c058bd31d47
Riceball LEE <snowyu.lee@gmail.com>
2025-03-29 18:48:02 +0800

    feat: add dryRun flag

--- src/oclif/commands/apply/index.ts
+++ src/oclif/commands/apply/index.ts
@@ -1,17 +1,14 @@
-import {Args, Command, Flags} from '@oclif/core'
 import path from 'path'
-import { applyTemplate, getInputDataBySchema, loadConfigFile } from '../../../lib/index.js'
-
-const DefaultTemplifyConfigFileName = '.templify.yaml'
-const DefaultDataFileName = 'templify-data'
+import {Args, Command, Flags} from '@oclif/core'
+import { applyTemplate, DefaultDataFileName, DefaultTemplifyConfigFileName, getInputDataBySchema, loadConfigFile } from '../../../lib/index.js'
 
 export default class ApplyCommand extends Command {
   static aliases = ['']
   static args = {
     template_dir: Args.directory({description: 'the template folder to apply', required: true, default: '.'}),
-    data: Args.string({description: 'the json data to apply. see also `--data data-file-path`'})
+    data: Args.string({description: 'the json data to apply. see also `--data data-file-path`'}),
   }
-  static description = 'Apply template data configuration etc to the template folder directly.'
+  static description = '🚀 Apply template data configuration etc to the template folder directly.'
   static examples = [
     `<%= config.bin %> <%= command.id %> .
 Appied. Enjoy your project at "."
@@ -20,6 +17,7 @@ Appied. Enjoy your project at "."
   static flags = {
     data: Flags.file({char: 'd', description: 'the data file to apply. see also DATA argument'}),
     interactive: Flags.boolean({char: 'i', description: 'interactive mode', default: true, allowNo: true}),
+    dryRun: Flags.boolean({char: 'n', description: 'dry run mode'}),
   }
 
   async run(): Promise<void> {
@@ -46,10 +44,12 @@ Appied. Enjoy your project at "."
     }
 
     const schema = {
+      title: 'Templify: Instantly convert project into template',
       ...tempifyConfig,
       type: 'object',
       properties: tempifyConfig.parameters,
     }
+    const dryRun = flags.dryRun ?? tempifyConfig.dryRun
 
     data = await getInputDataBySchema(schema, {
       rootDir,
@@ -57,10 +57,11 @@ Appied. Enjoy your project at "."
       data,
       nonInteractive,
       defaultDataFileName: DefaultDataFileName,
+      dryRun,
     })
 
     if (data) {
-      await applyTemplate(rootDir, {...tempifyConfig, parameters: data})
+      await applyTemplate(rootDir, {...tempifyConfig, parameters: data, dryRun})
     } else {
       this.error('No data found. Please create a data file at ./templify-data.json')
     }
