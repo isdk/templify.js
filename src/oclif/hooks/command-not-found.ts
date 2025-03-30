4ade344748d5b66ac72e39cae0d4733ee0c8c5b6
Riceball LEE <snowyu.lee@gmail.com>
2025-03-30 19:07:27 +0800

    fix: can not pass data argument by default

--- src/oclif/hooks/command-not-found.ts
+++ src/oclif/hooks/command-not-found.ts
@@ -1,7 +1,7 @@
 import type { Config, Hook } from '@oclif/core'
 
 export async function commandNotFound(this: Hook.Context, options: {id: string, argv: any[], userConfig: any, config: Config}) {
-  return options.config.runCommand('apply', [options.id, ...options.argv])
+  return options.config.runCommand('apply', [...options.id.split(':'), ...options.argv])
 }
 
 export default commandNotFound
