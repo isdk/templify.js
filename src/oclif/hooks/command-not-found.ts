04a6f9f6506b770926d97acf90d8e48707df946c
Riceball LEE <snowyu.lee@gmail.com>
2025-08-03 09:52:06 +0800

    fix: avoid recursive loop if apply command failed

--- src/oclif/hooks/command-not-found.ts
+++ src/oclif/hooks/command-not-found.ts
@@ -1,6 +1,7 @@
 import type { Config, Hook } from '@oclif/core'
 
 export async function commandNotFound(this: Hook.Context, options: {id: string, argv: any[], userConfig: any, config: Config}) {
+  if (options.id?.startsWith('apply')) throw new Error('Can not find command: ' + options.id + '!')
   return options.config.runCommand('apply', [...options.id.split(':'), ...options.argv])
 }
 
