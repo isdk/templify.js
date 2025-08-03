ce7f270c4e864139db80192307933041e2036b14
Riceball LEE <snowyu.lee@gmail.com>
2025-08-03 09:54:40 +0800

    refactor: disable warnings for ExperimentalWarning

--- bin/dev.js
+++ bin/dev.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S npx --node-options='--trace-warnings' tsx
+#!/usr/bin/env -S npx --node-options='--trace-warnings --no-warnings=ExperimentalWarning' tsx
 // #!/usr/bin/env -S node --loader ts-node/esm --disable-warning=ExperimentalWarning
 
 import {execute} from '@oclif/core'
